e246a7450db4f9a97cc162a358223f2f7a1a3d73
Vitaliy <v.penkovskiy@gmail.com>
2024-04-11 07:34:52 +0300

    Modal_step_0.1

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import { Products } from './components/Products';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal } from './components/Modal';
 
 function App() {
   return (
     <div className="App">
+      <Modal />
       <NavBar />
       <Banner />
       <Products />
---
+++ src/components/Modal.js
@@ -0,0 +1,10 @@
+//import { useState, useEffect } from 'react';
+export const Modal = () =>{
+    return(
+        <div className="modal-container">
+            <div className="modal-wind">
+                <p>Text</p>
+            </div>
+        </div>
+    )
+}
